762b3adc4da2a1f177afcdd70f04faefad4a24b2
QuentinHsuow <quentinhsuow@gmail.com>
2023-04-05 16:29:41 +0800

    [#12] design: complete main logic of sending message

--- src/components/chat/chat_board.tsx
+++ src/components/chat/chat_board.tsx
@@ -1,13 +1,42 @@
+import { getSocket } from "@/utils/websocket";
+import {useState} from "react";
+import {useCookies} from "react-cookie";
 
-const ChatBoard = () => {
+interface ChatBoardProps {
+    sessionId: number;
+}
 
+const ChatBoard = (props: ChatBoardProps) => {
 
+    const socket = getSocket();
+    const [cookie, setCookie] = useCookies(["id"]);
+    const [messages, setMessages] = useState([]);
+    const id = cookie.id;
 
-
+    socket.emit("pull", {"sessionId": props.sessionId, "messageScale": 30});
+    socket.on("pull", (res) => {
+        setMessages(res.messages);
+    })
+    socket.on("send", (res) => {
+        if(res.sessionId === props.sessionId) {
+            setMessages(messages => [...messages, {
+                "senderId": res.senderId,
+                "timestamp": res.timestamp,
+                "message": res.message,
+            }]);
+        }
+    })
 
     return (
         <div>
-
+            {messages.map((message) => (
+                <div>
+                    <img src={`api/session/img/${message.senderId}`} alt={"Loading..."}/>
+                    <text>
+                        {message.message}
+                    </text>
+                </div>
+            ))}
         </div>
     )
 };
