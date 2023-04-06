a51a8dc11aa6a7c36ea9519cb78a953e5fa813a3
QuentinHsuow <quentinhsuow@gmail.com>
2023-04-06 16:32:02 +0800

    [#12] improve profile

--- src/components/settings/profile.tsx
+++ src/components/settings/profile.tsx
@@ -1,11 +1,49 @@
 import styles from "@/components/settings/setting.module.css";
 import { useCookies } from "react-cookie";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import { request } from "@/utils/network";
-import { Image, message } from "antd";
+import { Image, message, Upload } from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import type { UploadChangeParam } from 'antd/es/upload';
+import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
+
+const getBase64 = (img: any, callback: (url: string) => void) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result as string));
+    reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file: RcFile) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+        message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+        message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+};
 
 const Profile = () => {
     const [cookie, setCookie] = useCookies(['id']);
+    const [loading, setLoading] = useState(false);
+    const [imgUrl, setImgUrl] = useState<string>();
+
+    const handleChange: UploadProps['onChange'] = (info: UploadChangeParam<UploadFile>) => {
+        if (info.file.status === 'uploading') {
+            setLoading(true);
+            return;
+        }
+        if (info.file.status === 'done') {
+            // Get this url from response in real world.
+            getBase64(info.file.originFileObj as RcFile, (url) => {
+                setLoading(false);
+                setImageUrl(url);
+            });
+        }
+    };
+
     const user_id = cookie.id;
     let response;
     useEffect(() => {
@@ -17,6 +55,7 @@ const Profile = () => {
     return (
         <div className={styles.profile}>
             <Image src={`api/session/img/${user_id}`} />
+
             <text></text>
         </div>
     );
